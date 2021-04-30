c555f2cf6103593f786c02ad1b1d498d74d067f5
Gabriel Almeida <gabriel.almeida@imagine.com.br>
2021-04-30 08:24:28 -0300

    demo

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using demo_mvc.Models;
+
+namespace demo_mvc.Controllers
+{
+    [Route("")]
+    [Route("gestao-clientes")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("")]
+        [Route("pagina-inicial")]
+        public IActionResult Index(string id, string categoria)
+        {
+            return View();
+        }
+
+        [Route("privacidade")]
+        [Route("politica-de-privacidade")]
+        public IActionResult Privacy()
+        {
+            // return Json("{'nome':'Gabriel'}");
+            // var fileBytes = System.IO.File.ReadAllBytes("/home/gabriel/Documentos/aquecimento.csv");
+            // var fileName = "arquivo.txt";
+            // return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+
+            // return Content("Qualquer coisa");
+
+            return View();
+        }
+
+        [Route("erro-encotrado")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
